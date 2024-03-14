6692568a887af13f03ccd811952eb3578a3169ce
gabzouz37 <88781950+gabfstr@users.noreply.github.com>
2024-03-14 02:03:14 +0100

    clean images & logo, add img meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
       options: {
         siteUrl: "https://www.gabriel.fiastre.fr", // Used for sitemap generation
         manifestSettings: {
-          favicon: "./content/images/draw_logo_contour.png", // Path is relative to the root
+          favicon: "./content/images/favicon.png", // Path is relative to the root
           siteName: "Gabriel Fiastre Portfolio", // Used in manifest.json
           shortName: "Gabriel Fiastre", // Used in manifest.json
           startUrl: "/", // Used in manifest.json
